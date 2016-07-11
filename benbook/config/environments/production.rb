ef6dd0173632e777c93cccb1a6a6ab44a2ac6139
benscanlan <bscanlan@mail.csuchico.edu>
2016-07-10 22:04:49 -0700

    push4

--- benbook/config/environments/production.rb
+++ benbook/config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+#ADDED BY ME
+#config.action_controller.relative_url_root = '/benbook'
 end
